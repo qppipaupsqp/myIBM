d327639bb684d8a3b708da9c8e6dd6c92a5e2e77
qppipaupsqp <112641894+qppipaupsqp@users.noreply.github.com>
2022-11-11 16:57:58 +0800

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Ben Manabat__ <br>
+# Data scientist
+
+# _I am interested with data science because apparently, it was the common factor of my I have been doing across my chaotic career history. I'm good at it, and I enjoy it_
+
+# ### Finding the hypotenuse
+
+# In[5]:
+
+
+import math
+a=3
+b=4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+
+# 1. Numbered List
+# > - Bulleted and indented
+# ***
+# External Link: __[Permalink](https://jp-tok.dataplatform.cloud.ibm.com/analytics/notebooks/v2/24100d94-aa3b-456f-8754-10d283b716d4/view?access_token=2e13300147bc4eeb09e9af8dc30336010fe3c583acd6a88a414e4f74926d1a9d)__ <br>
+# 
+# 
